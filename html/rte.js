41ec828bc7e1ac45070fc4f7eabf350a6a6f8ac8
kenyu73 <kenyu73@9eeb6ff2-14b9-11df-af62-8f85de635dbf>
2010-03-16 03:52:23 +0000

    support for timemce RTE

---
+++ html/rte.js
@@ -0,0 +1,17 @@
+// configuration information for TinyMCE
+//
+
+tinyMCE.init({
+	theme : "advanced",
+	mode: "exact",
+	elements : "text",
+	theme_advanced_toolbar_location : "top",
+	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,"
+	+ "justifyleft,justifycenter,justifyright,separator,"
+	+ "bullist,numlist,separator,"
+	+ "link,unlink,image,separator,"
+	+ "code,separator,charmap",
+	theme_advanced_buttons2 : "",
+	height:"130px",
+	width:"99%"
+});
